247d3a9bdd596b091361fc162b5806724cd8fc0f
e <1107719932@qq.com>
2020-06-25 21:56:29 +0800

    mulprocess
    Signed-off-by: e <1107719932@qq.com>

---
+++ pythontools/mulprocess.py
@@ -0,0 +1,21 @@
+from multiprocessing import Pool
+import time, random
+
+
+def long_time_task(name):
+    print('Run task %s ...' % (name))
+    start = time.time()
+    time.sleep(random.random() * 3)
+    end = time.time()
+    print('Task %s runs %0.2f seconds.' % (name, (end - start)))
+
+
+if __name__=='__main__':
+    print('Parent process.')
+    p = Pool(4)
+    for i in range(5):
+        p.apply_async(long_time_task, args=(i,))
+    print('Waiting for all subprocesses done...')
+    p.close()
+    p.join()
+    print('All subprocesses done.')
